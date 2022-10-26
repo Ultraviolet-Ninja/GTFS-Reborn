2655729037a185a9047f30a1d02c92f93584a6c1
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-10-25 23:13:10 -0500

    Going down to Java 17

--- src/main/java/jasmine/jragon/gtfsreborn/Driver.java
+++ src/main/java/jasmine/jragon/gtfsreborn/Driver.java
@@ -1,4 +1,21 @@
 package jasmine.jragon.gtfsreborn;
 
-public class Driver {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Driver extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Driver.class.getResource("display.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
